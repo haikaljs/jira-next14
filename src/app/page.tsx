461e6747a5c252d9cf0318e033e558b6257e087e
haikaljs <haikaljavascript@gmail.com>
2024-10-08 18:12:06 +0800

    Customizing components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <div>
-      <Button variant="destructive" size="lg">
-        Click me
-      </Button>
-      <p className="font-semibold">Haikal</p>
+    <div className="flex gap-4">
+      <Input/>
+      <Button size="lg">Primary</Button>
+      <Button variant="secondary" size="sm">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">Muted</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="teritary">Teritary</Button>
     </div>
   );
 }
